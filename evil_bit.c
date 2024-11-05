1feb1e8f1d220c1720aa43571c06dbf70cc86321
Mikk Margus Möll <mikk@you.die.ifyou.work>
2024-11-05 21:56:54 +0200

    drop __always_inline, split out IPv4 checking

--- evil_bit.c
+++ evil_bit.c
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <stdbool.h>
 
 #include <linux/bpf.h>
 #include <linux/ip.h>
@@ -12,49 +13,53 @@
 static const int l3_off = L3_OFF;
 static const int l3_len = sizeof(struct iphdr);
 
+static bool is_ipv4(struct __sk_buff *skb);
 static int set_evil_bit(struct __sk_buff *skb);
 
 SEC("egress")
 int egress_handler(struct __sk_buff *skb)
 {
-    if (bpf_skb_pull_data(skb, l3_off + l3_len) < 0)
+    if (!is_ipv4(skb))
+    {
         return TC_ACT_OK;
+    }
+    return set_evil_bit(skb);
+}
+
+static bool is_ipv4(struct __sk_buff *skb)
+{
+    if (bpf_skb_pull_data(skb, l3_off + l3_len) < 0)
+        return false;
 
     void *data_end = (void *)(uint64_t)skb->data_end;
     void *data = (void *)(uint64_t)skb->data;
 
     // not enough data
     if (data + l3_off + l3_len > data_end)
-        return TC_ACT_OK;
+        return false;
 
     uint16_t ether_type;
     uint8_t version;
 
     switch (l3_off)
     {
-    case 0: // L3 tunnel (e.g wireguard/tun); check first 4 bits for IPv4 version field
+    case 0: // L3 tunnel (e.g wireguard or OpenVPN with tun); check first 4 bits for IPv4 version field
         version = *(uint8_t *)(data + l3_off);
-        if ((version >> 4) != 4)
-            return TC_ACT_OK;
-        break;
+        return (version >> 4) == 4;
     case 14:
     case 18:
         // ethernet (dot1q or otherwise)
         // ensure ethertype is IPv4
         ether_type = *(uint16_t *)(data + l3_off - 2);
         ether_type = bpf_ntohs(ether_type);
-        if (ether_type != ETH_P_IP)
-            return TC_ACT_OK;
-        break;
+        return ether_type == ETH_P_IP;
     default:
         // unknown type, ignore
-        return TC_ACT_OK;
+        return false;
     }
-
-    return set_evil_bit(skb);
 }
 
-__always_inline static int set_evil_bit(struct __sk_buff *skb)
+static int set_evil_bit(struct __sk_buff *skb)
 {
     struct iphdr *ip = (void *)(uint64_t)skb->data + l3_off;
     // bpf_skb_store_bytes expects a multiple of 4 bytes, so include the id field
